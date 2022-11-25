27c8578514f9b17e77072ed7e4abeb32438acd87
Michael <mickeymurage@gmail.com>
2022-11-25 19:19:20 +0300

    fixed error in number palindrome checker

--- numberPalindrome.js
+++ numberPalindrome.js
@@ -1,8 +1,9 @@
 var isPalindrome = function(x) {
-	let y = x.toString().split('')
-	let z = y.reverse()
+	let y = x.toString().split('').reverse().join('')
+	let z = x.toString()
 	
 	return y === z
 };
 
-console.log(isPalindrome(121));
+// console.log(isPalindrome(121));
+module.exports = isPalindrome;
