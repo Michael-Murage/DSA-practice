a06c6fb928e4c067d250447d171bbd6c964853f1
Michael <mickeymurage@gmail.com>
2022-11-25 20:26:45 +0300

    deleted

--- frogJump.spec.js
+++
@@ -1,5 +0,0 @@
-const solution = require('./frogJump')
-
-test("First", () => {
-	expect(solution(10, 85, 30)).toBe(3)
-})
--- numberPalindrome.js
+++
@@ -1,9 +0,0 @@
-var isPalindrome = function(x) {
-	let y = x.toString().split('').reverse().join('')
-	let z = x.toString()
-	
-	return y === z
-};
-
-// console.log(isPalindrome(121));
-module.exports = isPalindrome;
